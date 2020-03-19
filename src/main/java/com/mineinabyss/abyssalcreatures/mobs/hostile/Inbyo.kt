a09a1c3245779450198f95c486770465f78793dd
0ffz <dan.voznyy@gmail.com>
2020-03-18 22:41:50 -0400

    Add Inbyo rock throwing. Decrease Orb Piercer size for better pathfinding.

--- src/main/java/com/mineinabyss/abyssalcreatures/mobs/hostile/Inbyo.kt
+++ src/main/java/com/mineinabyss/abyssalcreatures/mobs/hostile/Inbyo.kt
@@ -5,32 +5,39 @@ import com.mineinabyss.abyssalcreatures.mobs.passive.Neritantan
 import com.mineinabyss.idofront.items.editItemMeta
 import com.mineinabyss.mobzy.mobs.behaviours.HitBehaviour
 import com.mineinabyss.mobzy.mobs.types.HostileMob
-import net.minecraft.server.v1_15_R1.*
+import com.mineinabyss.mobzy.pathfinders.hostile.ItemThrowable
+import com.mineinabyss.mobzy.pathfinders.hostile.PathfinderGoalThrowItems
+import net.minecraft.server.v1_15_R1.PathfinderGoalNearestAttackableTarget
+import net.minecraft.server.v1_15_R1.World
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack
+import org.bukkit.entity.LivingEntity
 import org.bukkit.inventory.ItemStack
+import kotlin.random.Random
 
-class Inbyo(world: World?) : HostileMob(world, "Inbyo"), HitBehaviour, IRangedEntity {
+class Inbyo(world: World?) : HostileMob(world, "Inbyo"), HitBehaviour, ItemThrowable {
     override fun createPathfinders() {
         super.createPathfinders()
         addTargetSelector(1, PathfinderGoalNearestAttackableTarget(this, Neritantan::class.java, true))
-        addPathfinderGoal(1, PathfinderGoalArrowAttack(this, 1.25, 40, 10.0f))
+        addPathfinderGoal(1, PathfinderGoalThrowItems(this, 0.0, 10.0))
     }
 
-    //TODO rewrite as our own pathfinder
-    override fun a(entityliving: EntityLiving, f: Float) {
-        val snowball = ThrownRock(world, this)
-        val dX = entityliving.locX() - locX()
-        val dY = entityliving.headY - 1 - snowball.locY()
-        val dZ = entityliving.locZ() - locZ()
-        val f1 = MathHelper.sqrt(dX * dX + dZ * dZ) * 0.2
-        a(SoundEffects.ENTITY_SNOW_GOLEM_SHOOT, 1.0f, 1.0f / (getRandom().nextFloat() * 0.4f + 0.8f))
-        snowball.shoot(dX, dY + f1, dZ, 1.6f, 12.0f)
+    override val itemToThrow = ItemStack(Material.SNOWBALL).editItemMeta {
+        setCustomModelData(3)
+    }
 
-        val itemStack = ItemStack(Material.SNOWBALL).editItemMeta {
-            setCustomModelData(3) //TODO fix coal pebble model
-        }
-        snowball.setItem(CraftItemStack.asNMSCopy(itemStack))
+    //TODO try not to rely on NMS at all here
+    //TODO move some of this code out so it's easier to reuse
+    override fun throwItem(target: LivingEntity) {
+        val snowball = ThrownRock(world, this)
+        val targetLoc = target.eyeLocation
+        val dX = targetLoc.x - this.locX
+        val dY = targetLoc.y - this.locY - 0.4
+        val dZ = targetLoc.z - this.locZ
+        living.location.world?.playSound(living.location, Sound.ENTITY_SNOW_GOLEM_SHOOT, 1.0f, 1.0f / (Random.nextDouble(0.8, 1.2).toFloat()))
+        snowball.shoot(dX, dY, dZ, 1.6f, 12.0f)
+        snowball.setItem(CraftItemStack.asNMSCopy(itemToThrow))
         world.addEntity(snowball)
     }
 }
